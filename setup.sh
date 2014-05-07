a335b846d90f83fb4ba3ca7ff4db83197c53fc25
dkeresteci <dkeresteci@gmail.com>
2014-05-07 18:27:53 -0400

    Removed nvm, added install for npm and yo
    Also generator express

--- setup.sh
+++ setup.sh
@@ -6,16 +6,23 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+#curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+#source $HOME/.nvm/nvm.sh
+#nvm install v0.10.12
+#nvm use v0.10.12
+
+#load npm
+sudo apt-get install -y npm
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 npm install -g jshint
+
+#Install yo and express generator
+npm install -g yo
+npm install -g generator-express
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
